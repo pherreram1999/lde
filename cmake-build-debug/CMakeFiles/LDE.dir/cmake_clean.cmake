f31a9c22105d16b5cc9e751b78848be8223851fa
sistemas <pherreram1600@alumno.ipn.mx>
2021-11-07 11:57:42 -0600

    first commit

---
+++ cmake-build-debug/CMakeFiles/LDE.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LDE.dir/main.cpp.o"
+  "LDE"
+  "LDE.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LDE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/LDE.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LDE.
+# This may be replaced when dependencies are built.
